53135be8ee252ec714a70c6d2b9150c9885d8981
Kalab Solomon <kalab27solomon@gmail.com>
2023-03-07 15:11:47 -0800

    adding task9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    res = abs(number) % 10
-    print("{}".format(res))
-    return (res)
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
