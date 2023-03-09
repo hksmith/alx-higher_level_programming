8601d873f90d909c31850ae444ede20b8fa02678
Kalab Solomon <kalab27solomon@gmail.com>
2023-03-09 10:16:15 -0800

    adding task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 a = 1
 b = 2
-res = add_0.add(a, b)
-print("{} + {} = ()", a, b, res)
+res = add(a, b)
+print("{} + {} = ()".format(a, b, res))
