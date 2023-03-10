bad3da5e795913241bea6ac2167c81da26281684
Kalab Solomon <kalab27solomon@gmail.com>
2023-03-09 22:53:26 -0800

    adding task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,13 @@
 if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) == 0:
+    count = len(sys.argv) - 1
+
+    if count == 0:
         print("0 arguments.")
-    elif len(sys.arv) == 1:
+    elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(len(sys.argv)))
+        print("{} arguments:".format(count))
     for i in range(len(sys.argv)):
-        print("{}: {}".format(i + 1, sys.argv[i]))
+        print("{}: {}".format(i + 1, count))
