3def650a832bee247b60a1de92e765693b222e27
Kalab Solomon <kalab27solomon@gmail.com>
2023-03-09 22:44:34 -0800

    adding task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+
+if len(sys.argv) == 0:
+    print("0 arguments.")
+elif len(sys.arv) == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(len(sys.argv)))
+for i in range(len(sys.argv)):
+    print("{}: {}".format(i + 1, sys.argv[i]))
