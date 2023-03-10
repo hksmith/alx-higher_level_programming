84e5eeec6bb9eff2a9c1f3195056c8d32bdd61ea
Kalab Solomon <kalab27solomon@gmail.com>
2023-03-09 22:47:01 -0800

    adding task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
-import sys
-
-if len(sys.argv) == 0:
-    print("0 arguments.")
-elif len(sys.arv) == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(len(sys.argv)))
-for i in range(len(sys.argv)):
-    print("{}: {}".format(i + 1, sys.argv[i]))
+    if len(sys.argv) == 0:
+        print("0 arguments.")
+    elif len(sys.arv) == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv)))
+    for i in range(len(sys.argv)):
+        print("{}: {}".format(i + 1, sys.argv[i]))
