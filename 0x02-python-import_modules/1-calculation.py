aef2f530f994a1c6f021d6fc3c80b22464838afb
Kalab Solomon <kalab27solomon@gmail.com>
2023-03-09 21:59:26 -0800

    adding task1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,9 +2,11 @@
 
 if __name__ == "__main__":
     """Print the calculation result of a and b"""
+    from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
-    from calculator_1 import add, sub, mul, div
+
     add(a, b)
     sub(a, b)
     mul(a, b)
