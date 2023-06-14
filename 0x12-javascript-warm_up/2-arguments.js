52a9ab0bdebca736535b1727aa1d0c4c7be9c1d1
Kalab Solomon <kalab27solomon@gmail.com>
2023-06-14 11:47:17 -0700

    adding task2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,7 @@
+if (process.argv.length === 0) {
+	console.log('No argument');
+} else if (process.argv.length === 1) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
