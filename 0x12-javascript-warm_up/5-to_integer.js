074ff135c7c526fb0862e55ae94f6bbcfc73354b
Kalab Solomon <kalab27solomon@gmail.com>
2023-06-14 12:46:08 -0700

    adding task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (!isNaN(process.argv[2]) || process.argv[2] !== undefined) {
-	console.log('My number:' + Number(process.argv[2]));
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+	console.log('Not a number');
 } else {
 	console.log('Not a number');
 }
