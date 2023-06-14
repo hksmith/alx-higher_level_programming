da65d448cefedb3f6180fcf2c220c49724ede761
Kalab Solomon <kalab27solomon@gmail.com>
2023-06-14 15:23:41 -0700

    adding task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,9 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   const x = Number(process.argv[2]);
-  for (let i = 0; i < x - 1; i++) {
-          console.log('X'.repeat(x));
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
   }
 }
